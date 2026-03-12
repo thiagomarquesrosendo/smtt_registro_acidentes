673bde4596066aeb526d4c1fb2fa5811df78cb15
THIAGO MARQUES ROSENDO <thiagomarquesrosendo@gmail.com>
2026-03-11 23:28:17 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  output: "export",
 };
 
 export default nextConfig;
